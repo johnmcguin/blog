fc10fd53b449899dd9a970c5023691e8215dfe36
John McGuin <johnmcguin88@gmail.com>
2023-09-09 14:43:29 -0600

    fix: only publish published posts to rss feed
    hopefully fix issue for tags formatting for importing blogs onto dev to

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -3,10 +3,11 @@ import { getCollection } from 'astro:content';
 import { siteConfig } from '@/site-config';
 import sanitizeHtml from 'sanitize-html';
 import MarkdownIt from 'markdown-it';
+import { isPublished } from '../utils/post';
 const parser = new MarkdownIt();
 
 export const GET = async () => {
-	const posts = await getCollection('post');
+	const posts = await getCollection('post', isPublished);
 
 	return rss({
 		title: siteConfig.title,
@@ -16,7 +17,7 @@ export const GET = async () => {
 			title: post.data.title,
 			description: post.data.description,
 			pubDate: post.data.publishDate,
-			tags: post.data.tags,
+			tags: post.data.tags.join(', '),
 			link: `blog/${post.slug}`,
 			content: sanitizeHtml(parser.render(post.body)),
 		})),
