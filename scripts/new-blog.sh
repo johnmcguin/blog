e7620fca33d420c32944e2d89319dedddec65881
John McGuin <johnmcguin88@gmail.com>
2023-09-12 18:54:16 -0600

    script: templatize new blog post script

--- scripts/new-blog.sh
+++ scripts/new-blog.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 # create a new markdown blog post under the convention of 
 # /content/post/{timestamp}-{name}/index.md
-mkdir "./src/content/post/$(date +%s)-$1" && touch "$_/index.md"
+SLUG=$1
+
+mkdir "./src/content/post/$(date +%s)-$SLUG" \
+&& cat <<EOF > "$_/index.md"
+---
+title: '$SLUG'
+slug: '$SLUG'
+description: ''
+publishDate: $(date +%Y-%m-%d)
+tags: []
+draft: true
+---
+EOF
