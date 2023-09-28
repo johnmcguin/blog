34ad4804a894d478b6a8a1ad58ac383d9180d4e6
John McGuin <johnmcguin88@gmail.com>
2023-09-28 14:59:12 -0600

    script: build title from slug

--- scripts/new-blog.sh
+++ scripts/new-blog.sh
@@ -2,11 +2,12 @@
 # create a new markdown blog post under the convention of 
 # /content/post/{timestamp}-{name}/index.md
 SLUG=$1
+TITLE=$(echo "$SLUG" | tr '-' ' ' | awk '{ for(i=1; i<=NF; i++) $i=toupper(substr($i,1,1)) tolower(substr($i,2)); }1')
 
 mkdir "./src/content/post/$(date +%s)-$SLUG" \
 && cat <<EOF > "$_/index.md"
 ---
-title: '$SLUG'
+title: '$TITLE' 
 slug: '$SLUG'
 description: ''
 publishDate: $(date +%Y-%m-%d)
