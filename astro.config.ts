6e207e408f106b605958fec98fd3d69cbf74a831
John McGuin <johnmcguin88@gmail.com>
2023-10-06 15:11:05 -0600

    umami

--- astro.config.ts
+++ astro.config.ts
@@ -5,34 +5,34 @@ import sitemap from '@astrojs/sitemap';
 import prefetch from '@astrojs/prefetch';
 import remarkUnwrapImages from 'remark-unwrap-images';
 import { remarkReadingTime } from './src/utils/remark-reading-time.mjs';
-import { remarkLastModified } from './src/utils/remark-modified-time.mjs'
+import { remarkLastModified } from './src/utils/remark-modified-time.mjs';
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://blog.waysoftware.dev',
-	markdown: {
-		remarkPlugins: [remarkUnwrapImages, remarkReadingTime, remarkLastModified],
-		remarkRehype: { footnoteLabelProperties: { className: [''] } },
-		shikiConfig: {
-			theme: 'dracula',
-			wrap: true,
-		},
-	},
-	image: {
-		service: sharpImageService(),
-	},
-	integrations: [
-		mdx({}),
-		tailwind({
-			applyBaseStyles: false,
-		}),
-		sitemap(),
-		prefetch(),
-	],
-	compressHTML: true,
-	vite: {
-		optimizeDeps: {
-			exclude: ['@resvg/resvg-js'],
-		},
-	},
+  site: 'https://blog.waysoftware.dev',
+  markdown: {
+    remarkPlugins: [remarkUnwrapImages, remarkReadingTime, remarkLastModified],
+    remarkRehype: {
+      footnoteLabelProperties: {
+        className: ['']
+      }
+    },
+    shikiConfig: {
+      theme: 'dracula',
+      wrap: true
+    }
+  },
+  image: {
+    service: sharpImageService()
+  },
+  integrations: [mdx({}), tailwind({
+    applyBaseStyles: false
+  }), sitemap(), prefetch(), partytown()],
+  compressHTML: true,
+  vite: {
+    optimizeDeps: {
+      exclude: ['@resvg/resvg-js']
+    }
+  }
 });
